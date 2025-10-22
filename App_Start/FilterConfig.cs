05b1841fa91d86eef98e82231461918d8e6d67ed
nguyentruong-06 <nguyentruongg393@gmail.com>
2025-10-22 11:19:50 +0700

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _24dh112968_MyStore_lap1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_24dh112968_MyStore_lap1.MvcApplication" Language="C#" %>
